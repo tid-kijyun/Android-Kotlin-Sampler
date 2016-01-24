8dbec5cfe774ecac174df23d40f2210a5f420333
tid <tid.develop@gmail.com>
2016-01-25 05:11:45 +0900

    Converted the Java project to Kotlin

---
+++ app/src/main/java/com/tid/android_kotlin_sampler/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tid.android_kotlin_sampler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
