6658448ce5848d189f1d7891244cd43bf01ae144
tid <tid.develop@gmail.com>
2016-01-26 22:58:42 +0900

    Added click listener to button

--- app/src/main/java/com/tid/android_kotlin_sampler/MainActivity.kt
+++ app/src/main/java/com/tid/android_kotlin_sampler/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tid.android_kotlin_sampler
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.my_button) as Button
+        button.setOnClickListener {
+            button.text = "PRESSED!"
+        }
     }
 }
